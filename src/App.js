2e21c34f4bd437f3ce0ac990f8e71d5270cacbfb
Olamilekan <olamilekanomosanya@gmail.com>
2022-09-15 15:25:41 +0100

    rename app.css to app.css.module and add css rules for topbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import classes from'./App.module.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <nav className={classes.topbar}>
+          <img src='https://www.marketplace.org/wp-content/uploads/2019/07/ama2.png?resize=740%2C204'alt=''></img>
+        </nav>
+
         
       </header>
     </div>

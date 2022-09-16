4d307024247b03798d485944e3b4fa487cfc7f00
Olamilekan <olamilekanomosanya@gmail.com>
2022-09-16 17:38:58 +0100

    styling app.js

--- src/App.js
+++ src/App.js
@@ -23,10 +23,16 @@ function App() {
 
           <h3 className={classes.SectionHeading}>Select Color :</h3>
           <div> 
-            <img className= {classes.ProductImage} src='https://imgur.com/iOeUBV7.png' alt='black colored watch'></img>            
+            <img className= {[classes.ProductImage, classes.SelectedProductImage].join(' ')} src='https://imgur.com/iOeUBV7.png' alt='black colored watch'></img>            
             <img className= {classes.ProductImage} src='https://imgur.com/PTgQlim.png' alt='red colored watch'></img>
             <img className= {classes.ProductImage} src='https://imgur.com/Mplj1YR.png' alt='blue colored watch'></img>
             <img className= {classes.ProductImage} src='https://imgur.com/xSIK4M8.png' alt='purple colored watch'></img>
+          </div>
+
+          <h3 className={classes.SectionHeading}>Featured</h3>
+          <div>
+            <button className= {classes.FeatureItem}>Time</button>
+            <button className= {classes.FeatureItem}>Heart Rate</button>
           </div>
 
         </div>
